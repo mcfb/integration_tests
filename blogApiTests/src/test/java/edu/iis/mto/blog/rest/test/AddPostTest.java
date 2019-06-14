89da525696503990f15cd0404d34ddeb9e1179cc
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-06-14 20:36:15 +0200

    3.4.4 test for searching posts

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.blog.rest.test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.hamcrest.CoreMatchers;
 import org.json.JSONObject;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -12,6 +13,8 @@ public class AddPostTest extends FunctionalTests {
     private static final String POST_API_CONFIRMED = "blog/user/1/post";
     private static final String POST_LIKE_CONFIRMED = "/blog/user/1/like/2";
     private static final String POST_LIKE_AUTHOR = "/blog/user/1/like/1";
+    public static final String POST_SEARCH_USER_REMOVED = "/blog/user/4/post";
+    public static final String POST_SEARCH_VALID = "/blog/user/1/post";
 
     @Test
     public void userWithStatusConfirmedShouldBeAbleToAddPost() {
@@ -80,6 +83,46 @@ public class AddPostTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_BAD_REQUEST)
                 .when()
                 .post(POST_LIKE_AUTHOR);
+    }
+
+
+    @Test
+    public void shouldReturnErrorIfSearchingForDeletedTests(){
+
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .get(POST_SEARCH_USER_REMOVED);
+    }
+
+
+    @Test
+    public void searchingForPostsShouldReturnProperAmountOfLikes(){
+
+
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("likesCount", CoreMatchers.hasItem(0))
+                .when()
+                .get(POST_SEARCH_VALID);
+
     }
 
 
