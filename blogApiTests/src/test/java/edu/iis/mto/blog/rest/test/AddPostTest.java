4e8262ae971edae567cc02ed9f41f221b4628b6d
Fabicki.Maciej <209305@edu.p.lodz.pl>
2019-06-14 20:28:07 +0200

    3.4.3 test for liking posts

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddPostTest.java
@@ -10,9 +10,11 @@ import org.junit.Test;
 public class AddPostTest extends FunctionalTests {
 
     private static final String POST_API_CONFIRMED = "blog/user/1/post";
+    private static final String POST_LIKE_CONFIRMED = "/blog/user/1/like/2";
+    private static final String POST_LIKE_AUTHOR = "/blog/user/1/like/1";
 
     @Test
-    public void userWithStatusConfirmedShouldBeAbleToAddPost(){
+    public void userWithStatusConfirmedShouldBeAbleToAddPost() {
         JSONObject jsonObj = new JSONObject().put("entry", "test");
         RestAssured.given()
                 .accept(ContentType.JSON)
@@ -24,6 +26,60 @@ public class AddPostTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_CREATED)
                 .when()
                 .post(POST_API_CONFIRMED);
+    }
+
+
+    @Test
+    public void authorCannotLikeOwnPosts() {
+
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(POST_LIKE_AUTHOR);
+
+    }
+
+    @Test
+    public void userWithConfirmedStatusCanLikePost() {
+
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(POST_LIKE_CONFIRMED);
+
+    }
+
+    @Test
+    public void CannotLikePostMoreThanOnce() {
+
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(POST_LIKE_AUTHOR);
     }
 
 
